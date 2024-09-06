d67a1e9f36fcda89fb61bdc0a7cd401eecb4db93
Rozali-Ab <rozali_ab@outlook.com>
2024-09-06 19:56:45 +0300

    add RoomsCheckbox component

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -2,14 +2,13 @@
   import { ref, watch } from 'vue';
 
   import RangeSlider from "./RangeSlider.vue";
+  import RoomsCheckbox from "./RoomsCheckbox.vue";
 
   import { useFlatsFilter } from "../composables/useFlatsFilter.js";
   import router from "../router/index.js";
   import { DEFAULT_FILTER_PARAMETERS } from "../constants/filterParameters.js";
 
   const isFilterVisible = ref(false);
-
-  const roomsValues = ['s', '1', '2', '3'];
 
   const {filterParams} = useFlatsFilter();
 
@@ -39,11 +38,6 @@
 
     return queryParams;
   }
-
-
-  const isSelected = (room) => {
-    return rooms.value.includes(room);
-  };
 
   const onApplyFilter = () => {
 
@@ -90,22 +84,10 @@
           v-if="isFilterVisible"
           @click="isFilterVisible = false"
       >X</button>
-      <div class="filter__rooms">
-        <div class="filter__rooms__title title">комнаты</div>
-        <div class="filter__rooms__buttons">
-          <label v-for="room in roomsValues" :key="room">
-            <input type="checkbox" :value="room" v-model="rooms">
-            <span class="filter__rooms__checkbox" :class="{ selected: isSelected(room) }">
-              <template v-if="room === 's'">
-                {{room.toUpperCase()}}
-              </template>
-              <template v-else>
-                {{room}}k
-              </template>
-            </span>
-          </label>
-        </div>
-      </div>
+
+      <RoomsCheckbox
+          v-model:selected-rooms="rooms"
+      />
 
       <div class="separator"></div>
 
@@ -245,44 +227,6 @@
         border-radius: 10px;
       }
     }
-
-
-    &__rooms {
-
-      &__buttons {
-        display: flex;
-        flex-direction: row;
-
-        input[type="checkbox"] {
-          position: absolute;
-          opacity: 0;
-        }
-      }
-        &__checkbox {
-          display: inline-flex;
-          align-items: center;
-          justify-content: center;
-          margin-right: 5px;
-          margin-bottom: 10px;
-          width: 47px;
-          height: 40px;
-          font-size: 16px;
-          font-weight: 700;
-          border: 1px solid #D8D8D8;
-          border-radius: 5px;
-          background-color: #fff;
-          color: #2C323A;
-          user-select: none;
-          text-align: center;
-          cursor: pointer;
-
-          &.selected {
-            background-color: #70D24E;
-            color: #fff;
-          }
-        }
-    }
-
 
     &__area {
 
