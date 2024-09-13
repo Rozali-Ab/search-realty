20956749c637c5b9c164a7688f4aa2a8b83a0929
Rozali-Ab <rozali_ab@outlook.com>
2024-09-13 18:32:19 +0300

    типа запрос к серверу

---
+++ src/mock/fetchFlats.js
@@ -0,0 +1,11 @@
+import { data } from "./mock.json";
+
+export const fetchFlats = () => {
+
+  return new Promise((resolve) => {
+
+    setTimeout(() => {
+      resolve(data);
+    }, 500)
+  })
+}
