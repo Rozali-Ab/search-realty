661da89c73d7a1c9efd926123e0f0c736a8d916d
Rozali-Ab <rozali_ab@outlook.com>
2024-09-13 18:36:15 +0300

    TransitionGroup
    не плавный какой-то

--- src/views/FlatsView.vue
+++ src/views/FlatsView.vue
@@ -1,21 +1,33 @@
 <script setup>
-  import FlatsList from "../components/FlatsList.vue";
   import { useFlatsFilter } from "../composables/useFlatsFilter.js";
+  import FlatsList from "../components/FlatsList.vue";
+  import ListSkeleton from "../components/ListSkeleton.vue";
 
-  const { filteredFlats } = useFlatsFilter();
+  const { filteredFlats, isLoading } = useFlatsFilter();
 
 </script>
 
 <template>
-<FlatsList v-if="filteredFlats" :flats="filteredFlats" />
-<div v-else class="no-matches">
-  Объявления по вашему запросу не найдены. <br>Попробуйте изменить параметры поиска
-</div>
+<TransitionGroup name="list" >
+
+  <ListSkeleton v-if="isLoading" key="skeleton" :length="4"/>
+
+  <FlatsList v-else-if="filteredFlats.length" :flats="filteredFlats" />
+
+  <div v-else class="no-matches">
+    Объявления по вашему запросу не найдены. <br>Попробуйте изменить параметры поиска
+  </div>
+</TransitionGroup>
 </template>
 
 <style scoped lang="scss">
 .no-matches {
   padding: 30px;
   font-size: 1.5rem;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
 }
 </style>
