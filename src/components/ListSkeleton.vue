79a551ca48f600816267daee784cb0659b1c7efb
Rozali-Ab <rozali_ab@outlook.com>
2024-09-13 18:33:03 +0300

    скелетоны

---
+++ src/components/ListSkeleton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="list">
+    <FlatSkeleton v-for="item in props.length" :key="item"></FlatSkeleton>
+  </div>
+</template>
+
+<script setup>
+  import FlatSkeleton from "./FlatSkeleton.vue";
+
+  const props = defineProps({
+    length: {
+     type: Number,
+      default: 8,
+    }
+  })
+</script>
+
+<style scoped lang="scss">
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 1140px;
+    margin: 0 auto;
+
+    @media (max-width: 1200px) {
+      max-width: 855px;
+    }
+
+    @media (max-width: 900px) {
+      max-width: 570px;
+    }
+  }
+</style>
